b64f4e83f324e94095db8a646fd3bbfba84fb98b
Sanny <dugna1234@gmail.com>
2024-10-15 02:58:08 +0400

    [bugfix]: Fixed the module so that it actually sends mails now. All of the credentials must be written in .env

--- services/mail_sender.py
+++ services/mail_sender.py
@@ -1,12 +1,17 @@
 import smtplib
+import os
+from dotenv import load_dotenv
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from utils.mail_formatter import mail_format
 
-def send_email_notification(job_type, num_jobs, website, recipient_email):
-    sender_email = "your-email@gmail.com"
+load_dotenv()
+
+def send_email_notification(job_type, num_jobs, website, recipient_email, found_jobs):
+    sender_email = os.getenv('SENDER_EMAIL')
 
     subject = f"New {job_type} Jobs Notification"
-    body = f"Here are {num_jobs} new {job_type} jobs from {website}."
+    body = f"Here are {num_jobs} new {job_type} jobs from {website}." + mail_format(found_jobs)
 
     message = MIMEMultipart()
     message["From"] = sender_email
@@ -16,6 +21,7 @@ def send_email_notification(job_type, num_jobs, website, recipient_email):
 
     try:
         server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.login(sender_email, os.getenv('SENDER_PASS'))
         server.sendmail(sender_email, recipient_email, message.as_string())
         server.quit()
         print(f"Email sent to {recipient_email}")
