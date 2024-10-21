55da74029d7ae5af31998378ea57a8d7000e2918
Sanny <dugna1234@gmail.com>
2024-10-22 01:54:37 +0400

    [fix] Update mail_formatter so that it works with the renewed mail_sender

--- utils/mail_formatter.py
+++ utils/mail_formatter.py
@@ -1,8 +1,9 @@
 '''Module which contains the function that formats the fetched jobs as a string for the mail_sender.'''
 
 def mail_format(jobs_list):
-    formatted = [f'''The company {job[1]} has a position open for: {job[2]}. This was opened
-                 on {job[3]} and the deadline is {job[4]}. Link for more details: {job[5]} \n'''
-                 for job in jobs_list]
-    return ''.join(formatted)
-
+    if jobs_list:
+        formatted = [f'''The company {job[1]} has a position open for: {job[2]}. This was opened
+                    on {job[3]} and the deadline is {job[4]}. Link for more details: {job[5]} \n'''
+                    for job in jobs_list]
+        return ''.join(formatted)
+    return None
